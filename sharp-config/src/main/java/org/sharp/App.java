6c9ae6847207402694fcbe91f2b28903c50385d1
zhishanlingyun <liuli307@sina.com>
2018-06-16 01:14:45 +0800

    init

---
+++ sharp-config/src/main/java/org/sharp/App.java
@@ -0,0 +1,13 @@
+package org.sharp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
